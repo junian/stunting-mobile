1bdc93c9b9ab14c84dccb2e6e975a8b99b146957
Muhammad Haris Kumala <hariskumala31@gmail.com>
2023-09-17 11:20:37 +0700

    update tujuan api

--- lib/helper/Constant.dart
+++ lib/helper/Constant.dart
@@ -1,4 +1,4 @@
 class helper {
-  // static String api = "https://bebasstunting.alalanusantara.com/api";
-  static String api = "http://stantapp.alalanusantara.com/api";
+  static String api = "https://www.bebasstunting.com/api"; //Production
+  // static String api = "http://stantapp.alalanusantara.com/api";
 }
